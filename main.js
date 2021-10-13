a9ddafa91b77bd48083a197e4007c741e217fbbf
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-10-12 22:17:58 -0400

    data there, needs more styling

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
+const header = document.createElement("header"),
+    h1 = document.createElement("h1");
+h1.textContent = "Customer Directory";
+header.appendChild(h1);
+document.body.appendChild(header);
+
+
 const container = document.getElementById("container")
 console.log(container)
 
-const customer = customers[0]
-console.log(customer)
-const card = document.createElement('div')
-card.id = customer.email
+for (customer of customers) {
 
-const customerName = document.createElement('h3');
+    const card = document.createElement('div')
+    container.appendChild(card)
 
-customerName.innerText = customer.name.first + ' ' + customer.name.last
+    const customerImg = document.createElement('img')
+    customerImg.src=customer.picture.medium
+    card.appendChild(customerImg)
 
-container.appendChild(card)
+    const customerName = document.createElement('h3');
+    customerName.innerText = customer.name.first + ' ' + customer.name.last
+    card.appendChild(customerName)
 
-card.appendChild(customerName)
+    const customerEmail = document.createElement('p')
+    customerEmail.innerText = customer.email
+    card.appendChild(customerEmail)
 
-const customerEmail = document.createElement('p')
-customerEmail.innerText = customer.email
+    const customerAddress = document.createElement('p')
+    customerAddress.innerText = customer.location.street.number + ' ' + customer.location.street.name  
+    card.appendChild(customerAddress)
 
-// for (customer of customers) {
-//     const card = document.createElement('div')
-//     card.id = customer.email
-//     const customerName = document.createElement('h3');
-//     container.appendChild(card)
-//     customerEmail.innerText = customer.email
+    const customerCity = document.createElement('p')
+    customerCity.innerText = customer.location.city + ' ' + customer.location.state + ' ' + customer.location.postcode
+    card.appendChild(customerCity)
 
-// }
+    const customerDob = document.createElement('p')
+    customerDob.innerText = 'DOB: ' + moment(customer.dob.date).format("MMM DD,YYYY")
+    card.appendChild(customerDob)
+
+    const customerReg = document.createElement('p')
+    customerReg.innerText = 'Member since: ' + moment(customer.registered.date).format("MMM DD, YYYY")
+    card.appendChild(customerReg)
+}
 
